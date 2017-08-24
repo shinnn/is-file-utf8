95ab47ebdcf9b05b13b2253de2b98ffd1bf46190
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-08-24 19:40:56 +0900

    remove Promise polyfill
    [BREAKING] drop support for Node.js < 0.12

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 
 var isUtf8 = require('is-utf8');
-var PinkiePromise = require('pinkie-promise');
 var readChunk = require('read-chunk');
 
 module.exports = function isFileUtf8(path) {
-  return new PinkiePromise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
     readChunk(path, 0, 4, function(err, buf) {
       if (err) {
         reject(err);
